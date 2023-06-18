73acc080e4b8f910481ffe3af24000e53f9a408d
Carl Conroy <sprintman210@gmail.com>
2023-06-18 15:34:18 -0400

    Returning json in res

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// app.METHOD(path, HANDLER);
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    data = {
+        name: 'Carl',
+        isAwesome: true
+    };
+    res.json(data)
 });
 
 const port = 1234;
@@ -11,3 +17,4 @@ const port = 1234;
 app.listen(port, ()=>{
     console.log(`We are listening at http://localhost:${port}`)
 });
+
