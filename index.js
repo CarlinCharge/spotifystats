279dd563536345b405ceaaab762000723213b7e0
Carl Conroy <sprintman210@gmail.com>
2023-06-19 00:22:28 -0400

    add axios, build out post to req access token

--- index.js
+++ index.js
@@ -15,7 +15,23 @@ app.get('/', (req, res) =>{
     res.send('Welcome to Spotify Stats!');
 });
 
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
 app.get('/login', (req, res)=>{
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    const scope = 'user-read-privae user-read-email';
+
     const queryParams = querystring.stringify({
         client_id: CLIENT_ID,
         response_type: 'code',
@@ -24,9 +40,35 @@ app.get('/login', (req, res)=>{
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
+app.get('/callback', (req, res) => {
+  const code = req.query.code || null;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      if (response.status === 200) {
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      } else {
+        res.send(response);
+      }
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 
 app.listen(port, ()=>{
     console.log(`We are listening at http://localhost:${port}`)
 });
-
