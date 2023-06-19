84909d26d6ffc28f74396713ef93d092c2bd5134
Carl Conroy <sprintman210@gmail.com>
2023-06-19 18:36:05 -0400

    refresh token test

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 1234;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const axios = require('axios');
 
 
 
@@ -30,12 +31,13 @@ app.get('/login', (req, res)=>{
     const state = generateRandomString(16);
     res.cookie(stateKey, state);
 
-    const scope = 'user-read-privae user-read-email';
+    const scope = 'user-read-private user-read-email';
 
     const queryParams = querystring.stringify({
         client_id: CLIENT_ID,
         response_type: 'code',
-        redirect_uri: REDIRECT_URI
+        redirect_uri: REDIRECT_URI,
+        scope: scope
     })
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
@@ -58,7 +60,18 @@ app.get('/callback', (req, res) => {
   })
     .then(response => {
       if (response.status === 200) {
-        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+       const {access_token, token_type} = response.data
+
+        const { refresh_token } = response.data;
+
+        axios.get(`http://localhost:1234/refresh_token?refresh_token=${refresh_token}`)
+          .then(response => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch(error => {
+            res.send(error);
+          });
+
       } else {
         res.send(response);
       }
@@ -68,6 +81,29 @@ app.get('/callback', (req, res) => {
     });
 });
 
+
+app.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`We are listening at http://localhost:${port}`)
