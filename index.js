bb8970e3f099062281e1baa57ed241bcd93a2fa3
Carl Conroy <sprintman210@gmail.com>
2023-06-18 20:17:10 -0400

    add dotenv and environment variables for spotify api access

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
+require('dotenv').config()
 const express = require('express');
-
 const app = express();
+const port = 1234;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+
 
 // app.METHOD(path, HANDLER);
-
-app.get('/', (req, res) => {
-    data = {
-        name: 'Carl',
-        isAwesome: true
-    };
-    res.json(data)
-});
-
-app.get('/awesome-generator', (req, res) => {
-  const { name, isAwesome } = req.query;
-  res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
-  console.log('Awesome route hit')
-});
 
 app.get('/nodemon-test', (req, res) => {
    res.send('Nodemon works');
   console.log('Nodemon route hit');
 });
 
-//  http://localhost:1234/awesome-generator?name=Carl&isAwesome=true provide the following query params 
-// We are destructuring here 
 
-const port = 1234;
 
 app.listen(port, ()=>{
     console.log(`We are listening at http://localhost:${port}`)
